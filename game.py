4f754645707e8415b0f3401184f3b57f662510e7
Jan Papousek <papousek.honza@gmail.com>
2020-05-20 20:58:59 +0200

    Third release

--- game.py
+++ game.py
@@ -9,7 +9,12 @@ def bullscows():
         user_guess = bc_take_guess()
         if bc_incorrect_guess(user_guess) == False:
             continue
-        bc_evaluate(our_number, user_guess)
+        cows, bulls = bc_evaluate(our_number, user_guess)
+        bc_output(cows, bulls)
+        #funkce zaznamu pokusu
+        if bulls == 4:
+            print(f"Great job. It took you {log} times")
+            break
 
 def bc_intro():
     """Introduction for user"""
@@ -28,6 +33,7 @@ def bc_incorrect_guess(value):
     if not value.isdigit():
         print(f"ERROR: Your guess is not a number. You must enter a 4-digit integer.")
         return False
+
     elif not len(value) == 4:
         print(f"ERROR: Your guess has {len(value)} digits. You must enter a 4-digit integer.")
         return False
@@ -38,21 +44,12 @@ def bc_evaluate(number, guess):
     number_image = list(str(number))
     guess_image = list(str(guess))
     number = list(str(number))
-    # unsolved = list(range(0,len(guess)))
-    # unsolved = number
 
     for index,digit in enumerate(guess):
         if digit == number[index]:
             bulls += 1
-            # unsolved.remove(index)
             number_image.remove(digit)
             guess_image.remove(digit)
-
-    print(bulls, cows, number_image, guess_image)
-    # for nmb in image:
-    #     if nmb in guess:
-    #         cows += 1
-    #         image.remove(nmb)
 
     for digit in reversed(number_image):
         if digit in guess_image:
@@ -60,6 +57,19 @@ def bc_evaluate(number, guess):
             number_image.remove(digit)
             guess_image.remove(digit)
 
-    print(bulls, cows, number_image, guess_image)
+    return cows, bulls
+
+def bc_output(cows, bulls):
+    if bulls == 1:
+        bulls_name = "bull"
+    else:
+        bulls_name = "bulls"
+
+    if cows == 1:
+        cows_name = "cow"
+    else:
+        cows_name = "cows"
+
+    print(f"{bulls} {bulls_name}, {cows} {cows_name} ")
 
 bullscows()
